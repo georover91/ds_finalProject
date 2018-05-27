a4aad1724b25f1f307e0e7f15aa949bfdb39d8af
Bonik Ku <kubonik91@naver.com>
2018-05-27 15:03:36 +0900

    Stack 히스토리 저장객체와 DoubleLL 히스토리 저장객체를 함께 사용.
    oper과 key값의 히스토리를 저장하는 객체를 DoubleLL로 바꿈.
    myCurrDbPtr의 히스토리를 저장하는 객체는 Stack을 그대로 유지.

---
+++ ds_finalProject11/ds_finalProject11/DoubleLL_.h
@@ -0,0 +1,234 @@
+#include "DoubleLL.h"
+
+
+
+// Class cnostructor & Class destructor & Class copy constructor //////////////////
+template <class _ItemType>
+DoubleLL<_ItemType>::DoubleLL()
+{
+	currentPos = NULL;
+	headerPtr = NULL;
+	trailerPtr = NULL;
+
+	headerPtr = new DoubleLLNode<_ItemType>;
+	trailerPtr = new DoubleLLNode<_ItemType>;
+	 
+	headerPtr->info; //= "This is header.";		//이 쌍따옴표 넣으려면 왜 오류 나지?
+	headerPtr->topPtr = NULL;
+	headerPtr->bottomPtr = trailerPtr;
+
+	trailerPtr->info; //= "This is trailer.";	//이 쌍따옴표 넣으려면 왜 오류 나지?
+	trailerPtr->topPtr = headerPtr;
+	trailerPtr->bottomPtr = NULL;
+
+	currentPos = headerPtr;
+}
+
+//template <class _ItemType>
+//DoubleLL<_ItemType>::DoubleLL(DoubleLL<_ItemType>& copiedDoubleLL)
+//{
+//
+//}
+
+//template <class _ItemType>
+//DoubleLL<_ItemType>::~DoubleLL()
+//{
+//
+//}
+///////////////////////////////////////////////////////////////////////////////
+
+
+
+// GoTo~ & ChangeCurrentPos & IsCurrentPos~ & WhatIs~ //////////////////////////////////////////////////////////
+template <class _ItemType>
+void DoubleLL<_ItemType>::GoToTop()
+{
+	currentPos = headerPtr;
+}
+
+template <class _ItemType>
+void DoubleLL<_ItemType>::GoToBottom()
+{
+	currentPos = trailerPtr->topPtr;
+}
+
+template <class _ItemType>
+void DoubleLL<_ItemType>::GoToBeforePos()
+{
+	if (currentPos == headerPtr) {	//currentPos == headerPtr이면 더이상 currentPos가 움직이지 않도록 하기.
+		return;
+	}
+
+	currentPos = currentPos->topPtr;
+}
+
+template <class _ItemType>
+void DoubleLL<_ItemType>::GoToNextPos()
+{
+	currentPos = currentPos->bottomPtr;
+
+	if (currentPos == trailerPtr) { //원소가 없으면 currentPos = headerptr;로 다시 초기화.
+		currentPos = headerPtr;		// 이경우는, 다음의 코딩과 경우가 같다. currentPos = trailerPtr->topPtr;
+	}
+}
+
+template <class _ItemType>
+bool DoubleLL<_ItemType>::IsBeforePosHeader()
+{
+	if (currentPos->topPtr == headerPtr) { 
+		return true;
+	}
+	else {
+		return false;
+	}
+}
+
+template <class _ItemType>
+bool DoubleLL<_ItemType>::IsNextPosTrailer()
+{
+	if (currentPos->bottomPtr == trailerPtr) {
+		return true;
+	}
+	else {
+		return false;
+	}
+}
+
+template <class _ItemType>
+DoubleLLNode<_ItemType>*  DoubleLL<_ItemType>::WhatIsCurrentPos()
+{
+	return currentPos;
+}
+
+template <class _ItemType>
+void  DoubleLL<_ItemType>::ChangeHeaderToNext()
+//changed가 가리키는 노드가 이 객체 Double Linked List에 삽입되어 있어야 한다.
+{
+	if (currentPos->bottomPtr == trailerPtr) {
+		return;
+	}
+	else {
+		headerPtr = headerPtr->bottomPtr;
+		return;
+	}
+}
+/////////////////////////////////////////////////////////////////////////////////
+
+
+
+// InsertLine & DeleteLine ///////////////////////////////////////////////
+template <class _ItemType>
+void DoubleLL<_ItemType>::InsertLine(_ItemType newline)
+//처음에 currentPos가 가리키는 노드의 다음 위치에 newline이 insert되도록 한다.
+{
+	DoubleLLNode<_ItemType>* newLinePtr;
+	newLinePtr = new DoubleLLNode<_ItemType>;
+	newLinePtr->info = newline;
+
+	newLinePtr->bottomPtr = currentPos->bottomPtr;
+	newLinePtr->topPtr = currentPos->bottomPtr->topPtr;
+	currentPos->bottomPtr = newLinePtr;
+	newLinePtr->bottomPtr->topPtr = newLinePtr;
+	currentPos = newLinePtr;	
+
+	return;
+}
+
+template <class _ItemType>
+void DoubleLL<_ItemType>::DeleteLine()
+//DeleteLine() 함수 자체에 삭제후, 그 삭제된 노드의 바로 전 노드로 위치를 옮기는 과정 포함됨.
+{
+	DoubleLLNode<_ItemType>* tempLine;
+	tempLine = currentPos;
+
+	currentPos->topPtr->bottomPtr = currentPos->bottomPtr;
+	currentPos->bottomPtr->topPtr = currentPos->topPtr;
+
+	currentPos = currentPos->topPtr;
+	delete tempLine;
+}
+
+template <class _ItemType>
+void DoubleLL<_ItemType>::Insert_with_Deleting_nextPos(_ItemType newline)
+{
+	if (myDbDoubleLL.IsNextPosTrailer()) {
+		myDbDoubleLL.InsertLine(newline);
+	}
+	else {		// 뒤로 갔다가 다시 검색해서 myCurrDb를 형성한 경우.
+		myDbDoubleLL.GoToNextPos();
+		myDbDoubleLL.DeleteLine();		//DeleteLine() 함수 자체에 삭제후, 그 삭제된 노드의 바로 전 노드로 위치를 옮기는 과정 포함됨.
+										//현재위치 이후의 모든 myDbHistory를 삭제한 후 새 myDb를 history에 삽입하도록 한다.
+		Insert_with_Deleting_nextPos(newline);	//이를 recursive 형태로 구현.
+	}
+}
+/////////////////////////////////////////////////////////////////////
+
+
+
+// FindItem & GetItem ///////////////////////////////////////////////
+template<class _ItemType>
+void DoubleLL<_ItemType>::FindItem(_ItemType item, bool& found)
+//Pre:	ItemType is a type for which the operators "<" and 
+//		"==" are defined.
+//		List is not empty.
+//Post:	If there is an element someItem whose key matches item's key, then found = true; otherwise, found = false. 
+//      If found, location contains the address of someItem; 
+//      otherwise, location contains the address of the logical successor of item.
+//		header 위치(currentPos)의 다음 위치부터 검색 시작.
+{
+	//DoubleLLNode<_ItemType>* temp = currentPos;
+
+	//GoToTop();
+	//currentPos = current->bottom;
+
+	bool moreToSearch = true;
+
+	location = listData;
+	found = false;
+	while (moreToSearch && !found)
+	{
+		if (item < location->info)
+			moreToSearch = false;
+		else if (item == location->info)
+			found = true;
+		else
+		{
+			location = location->next;
+			moreToSearch = (location != NULL);
+		}
+	}
+
+	//currentPos = temp;
+}
+
+template <class _ItemType>
+_ItemType DoubleLL<_ItemType>::GetItem()
+{
+	return currentPos->info;
+}
+//////////////////////////////////////////////////////////////////////////
+
+
+
+// Print /////////////////////////////////////////////////////////////////
+template <class _ItemType>
+void DoubleLL<_ItemType>::Print()
+//처음위치부터 프린트 하거나, currentPos 위치의 다음 위치부터 프린트함.
+{
+	DoubleLLNode<_ItemType>* temp = currentPos;
+	GoToTop();	// 이걸 써서 맨처음부터 프린트하고 싶으면, 이걸 main함수에 쓰는 게 좋음.
+
+	currentPos = currentPos->bottomPtr;
+
+	while (currentPos != trailerPtr) {
+		cout << currentPos->info << endl;
+		currentPos = currentPos->bottomPtr;
+	}
+
+	if ((currentPos == trailerPtr)) { //원소가 없으면 currentPos = headerptr;로 다시 초기화.
+		currentPos = headerPtr;
+	}
+
+	currentPos = temp;
+}
+////////////////////////////////////////////////////////////////////////////
---
+++ ds_finalProject11/ds_finalProject11/main.cpp
@@ -0,0 +1,175 @@
+// 1.	현재 프로젝트에서 자료구조를 짜놓은 구조상,
+//		Tree 객체에 InsertItem 또는 DeleteItem 등을 하여 원소가 바뀌면, preQue, inQue, postQue 모두 갱신해줘야 함!!!
+// 2.	
+/////////////////////////////////////////////////////////////////////////////
+#include "DbManager_.h"
+
+
+
+int main()
+{
+	//프로젝트 본문 시작.
+	Tree<ItemType> T1;
+
+	string command;
+	ItemType dataLine;
+	int numCommands;
+	int numData;
+
+	ifstream inFile1("in1.txt");	//초기 데이터 베이스는 파일(초기DB구성파일) 하나에서 읽어와 구성.
+	numCommands = 0;
+	numData = 0;
+	if (!inFile1.is_open()) {
+		cout << "데이터 베이스를 구성할 파일을 열 수 없습니다." << endl;
+	}
+	else {
+		getline(inFile1, command);	// '초기DB구성파일'의 한 줄을 명령으로 받아옴.
+		while (command != "Quit")
+		{
+			if (command == "InsertStrings") {
+				getline(inFile1, dataLine);
+				T1.InsertItem(dataLine);
+				numData++;
+				cout << numData << "th dataLine input: " << dataLine << endl;
+				numCommands++;
+				cout << numCommands << "th command  input: " << command << " completed." << endl;
+			}
+			//else if () {
+			//
+			//}
+			else {
+				getline(inFile1, dataLine);
+				dataLine = "\0";
+				numData++;
+				cout << numData << "th dataLine input: " << "Nullified!" << endl;
+				numCommands++;
+				command = "Command Error!";
+				cout << numCommands << "th command  input: " << command << endl;
+			}
+
+			getline(inFile1, command);
+		};
+		inFile1.close();
+	}
+
+	ofstream outFile1("out1.txt");		// 내가 찾은 문자열을 저장하는 텍스트 파일.
+	ofstream garbage("garbage.txt");	//그냥 cout으로만 출력하고 싶을 때, ofstream으로 출력되는 값을 한곳에 모아 출력하여, 버릴때 쓰는 출력 파일스트림.
+
+	cout << endl;
+	cout << "<<<" << "초기 데이터 베이스(0번째 검색)." << ">>>" << endl;
+	T1.Print(garbage);
+	cout << endl;
+
+
+	
+	// 본격적으로 string DB 검색 프로그램을 실행.
+	DbManager<ItemType> DbM(T1);
+
+	cout << endl;
+	cout << endl;
+	DbM.cumNum++;
+	cout << "<<<" << DbM.cumNum << "번째 명령." << ">>>" << endl;
+	cout << "프로그램 실행명령을 입력하세요: ";
+	getline(cin, DbM.command);
+	// (command의 history를 저장할 필요없다. 따라서 command를 저장할 backStack 또는 forwardStack을 만들 필요는 없다.)
+
+	while (DbM.command != "Quit")
+	{
+		if (DbM.command == "go back") {
+			DbM.goBackward();
+
+			cout << endl;
+			cout << "<<<" << DbM.searchNum << "번째 검색." << ">>>" << endl;
+			DbM.Print_myCurrDb(garbage);
+			cout << endl;
+		}
+		else if (DbM.command == "go forward") {
+			DbM.goForward();
+
+			cout << endl;
+			cout << "<<<" << DbM.searchNum << "번째 검색." << ">>>" << endl;
+			DbM.Print_myCurrDb(garbage);
+			cout << endl;
+		}
+		else if (DbM.command == "search") {
+			while (1) {
+				cout << "검색 연산자를 입력하세요: ";
+				getline(cin, DbM.oper);
+				if ((DbM.oper == "and") || (DbM.oper == "or") || (DbM.oper == "not")) {
+					break;
+				}
+			}
+			if (DbM.oper != "not") {
+				cout << "검색 키워드를 입력하세요: ";
+				getline(cin, DbM.key);
+			}
+			DbM.Search_in_myCurrDb();
+
+			cout << endl;
+			cout << "<<<" << DbM.searchNum << "번째 검색." << ">>>" << endl;
+			DbM.Print_myCurrDb(garbage);
+			cout << endl;
+		}
+		//else if () {
+		//
+		//}
+		else {
+			
+		}
+
+		cout << endl;
+		cout << endl;
+		DbM.cumNum++;
+		cout << "<<<" << DbM.cumNum << "번째 명령." << ">>>" << endl;
+		cout << "프로그램 실행명령을 입력하세요: ";
+		getline(cin, DbM.command);
+	};
+
+
+
+
+	
+
+
+
+	// 본격적이었던 string DB 검색 프로그램을 실행 종료.
+	// 프로젝트 본문 끝.
+
+
+
+	/* //초기DB구성파일 잘 읽혔는지 Test 시작.
+	T1.Print(garbage);
+	cout << endl;
+	garbage << endl;
+	T1.Print_Reverse(garbage);
+	cout << endl;
+	garbage << endl;
+	*/ ////초기DB구성파일 잘 읽혔는지 Test 끝.
+
+
+
+	/* //TreeType의 merge함수 Test 시작.
+	ofstream outFile("garbage.txt");
+
+	Tree<ItemType> tem1;
+	tem1.InsertItem("a");
+	tem1.InsertItem("b");
+	tem1.InsertItem("c");
+	tem1.Print(outFile);
+	cout << endl;
+
+	Tree<ItemType> tem2;
+	tem2.InsertItem("d");
+	tem2.InsertItem("e");
+	tem2.InsertItem("f");
+	tem2.Print(outFile);
+	cout << endl;
+	
+	Tree<ItemType> tem3;
+	tem1.Merge(tem2);
+	tem3 = tem1;
+	tem3.Print(outFile);
+	*/ //TreeType의 merge함수 Test 끝.
+
+	return 0;
+}
